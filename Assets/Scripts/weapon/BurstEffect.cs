58f215c8462872d6982a15dc0414f8adfd0d36e3
Andreas Dahl <andreas@hrdahl.dk>
2015-04-19 17:59:26 +0200

    Fixed burst

---
+++ Assets/Scripts/weapon/BurstEffect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BurstEffect : MonoBehaviour {
+	public float growth;
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Enemy")
+		{
+			other.gameObject.GetComponent<EnemyInterface>().doEffect();
+			other.gameObject.GetComponent<EnemyInterface>().death();
+			Destroy(other.gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.localScale += new Vector3(growth, growth, 1f);
+	}
+}
